328164d7fbe034679d70caac830d178e1b88bf4e
dawoodahmedbutt <dawoodahmedbutt@gmail.com>
2025-12-16 00:33:53 +0000

    feat(log): setup centralised logging config

---
+++ src/log_config.py
@@ -0,0 +1,26 @@
+import logging
+import os
+
+LOG_DIR = '04_Logs'
+LOG_FILE = os.path.join(LOG_DIR, 'dashboard_execution.log')
+
+def setup_logging():
+    os.makedirs(LOG_DIR, exist_ok=True)
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    file_handler = logging.FileHandler(LOG_FILE, mode='a')
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    if not logger.handlers:
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+    return logger
+
+logger = setup_logging()
